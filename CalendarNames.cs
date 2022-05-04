a9e93214c2736ae36946e77669f010a501b915f9
DGrothe-PhD <[dag_fs@t-online.de]>
2022-05-04 17:34:59 +0200

    adding some European languages

--- CalendarNames.cs
+++ CalendarNames.cs
@@ -12,12 +12,34 @@ public class monthnaming{
         { "de-DE", new string[] {
         "Januar", "Februar", "März", "April", "Mai", "Juni", 
         "Juli", "August", "September", "Oktober", "November", "Dezember"}},
-        { "fr-FR", new string[] {
+        {"nl", new string[] {
+        "januari", "februari", "maart", "april", "mei", "juni",
+        "juli", "augustus", "september", "oktober", "november", "december"
+        }},
+        { "fr", new string[] {
         "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", 
         "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"}},
         { "en-GB", new string[] {
         "January", "February", "March", "April", "May", "June", 
-        "July", "August", "September", "October", "November", "December"}}
+        "July", "August", "September", "October", "November", "December"}},
+        { "es", new string[] {
+        "Enero ", "Febrero ", "Marzo ", "Abril ", "Mayo ", "Junio ",
+        "Julio ", "Agosto ", "Septiembre ", "Octubre ", "Noviembre ", "Diciembre "}},
+        { "it", new string[] {
+        "gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno",
+        "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"}},
+        {"pt", new string[] {
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        }},
+        {"pl", new string[] {
+        "styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec",
+        "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"
+        }},
+        {"uk", new string[] {
+        "Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", 
+        "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"
+        }}
     };
     
     public static string Months(int number){
